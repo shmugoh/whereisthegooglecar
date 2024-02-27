296ba986091b77caac92e04cc8889961a070e7ed
Juan P <juan@shmugo.co>
2024-02-27 17:17:43 -0500

    Properly Mutate Data during Infinite Scroll
    ...horray!!

--- site/src/pages/index.tsx
+++ site/src/pages/index.tsx
@@ -1,35 +1,37 @@
 import { api } from "~/utils/api";
 import { CardSet } from "~/components/layout/entry/card";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function Home({}) {
   // set dates
   const currentDate = new Date();
   const [monthsBack, setMonthsBack] = useState(0);
-  const getMonth = currentDate.getMonth() + 1;
-  const getYear = currentDate.getFullYear().toString();
+  let getMonth = (currentDate.getMonth() + 1).toString();
+  let getYear = currentDate.getFullYear().toString();
+
+  const lastMonth = useRef("");
+  const tries = useRef(0);
 
   // infinite scroll
   const [page, setPage] = useState(1);
   const [cardSets, setCardSets] = useState([{ data: [], month: "", year: "" }]);
 
   // fetch data
-  const [data, setData] = useState({});
   const dataMutation = api.post.queryByFilter.useMutation({
     onSuccess(data) {
-      setData(data);
-      console.log(data);
+      // sets data to the mutation
+      dataMutation.data = data;
+      dataMutation.isSuccess = true;
     },
   });
 
   const fetchData = async () => {
     // set dates for query
     setMonthsBack(monthsBack + 1);
-    const currentDate = new Date();
     currentDate.setMonth(currentDate.getMonth() - monthsBack);
-    const getMonth = (currentDate.getMonth() + 1).toString();
-    const getYear = currentDate.getFullYear().toString();
+    getMonth = (currentDate.getMonth() + 1).toString();
+    getYear = currentDate.getFullYear().toString();
 
     dataMutation.mutate({
       company: "google",
@@ -37,14 +39,48 @@ export default function Home({}) {
       year: getYear,
     });
 
-    // append to cardSets
-    setCardSets([
-      ...cardSets,
-      { data: dataMutation, month: getMonth.toString(), year: getYear },
-    ]);
-    // increment page
-    setPage(page + 1);
+    console.log(
+      `Sending: ${getMonth.toString()}, ${lastMonth.current} - ${getYear}`,
+    );
   };
+
+  useEffect(() => {
+    console.log(
+      `Received: ${lastMonth.current} - ${getYear}, ${getMonth.toString()}`,
+    );
+
+    if (dataMutation.data) {
+      if (dataMutation.data.length === 0) {
+        void fetchData();
+        tries.current++;
+        return;
+      }
+
+      if (dataMutation.data.length > 0) {
+        console.log("Found Data!");
+        console.log(dataMutation.data);
+      }
+
+      // using this as a way to stop an infinite loop
+      // of the same month being spawned
+      if (lastMonth.current !== getMonth.toString() || tries.current != 0) {
+        // append to cardSets
+        setCardSets([
+          ...cardSets,
+          {
+            data: dataMutation.data,
+            month: getMonth.toString(),
+            year: getYear,
+          },
+        ]);
+        // increment page
+        setPage(page + 1);
+      }
+
+      lastMonth.current = getMonth.toString();
+      tries.current = 0;
+    }
+  }, [dataMutation]);
 
   // fetch data on mount/start
   useEffect(() => {
