c9fe8a19f3e5e6b056ebef1790b8334b8cc407b5
Juan P <juan@shmugo.co>
2024-03-04 03:07:57 -0500

    [WEB] Add Vercel Analytics

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 import { type AppType } from "next/app";
 
@@ -49,6 +50,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           />
         </div>
         <Foot />
+        <Analytics />
       </div>
     </>
   );
