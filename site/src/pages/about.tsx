3870f7713b6261d076226806cafc760012595c9f
Juan P <juan@shmugo.co>
2024-03-14 18:24:16 -0500

    [WEB] revert error & fix code prose
    This reverts commit d149aeaa770a66e1d2dbf1bc72f421cf530f989a.

--- site/src/pages/about.tsx
+++ site/src/pages/about.tsx
@@ -144,11 +144,8 @@ export default function about() {
 
           <span className="space-y-3.5">
             <h2 className="border-p scroll-m-20 text-3xl font-extrabold italic tracking-tight underline underline-offset-4">
-              Are you affiliated with the previous{" "}
-              <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-3xl font-semibold">
-                whereisthegooglecar.com
-              </code>{" "}
-              or any of its owners?
+              Are you affiliated with the previous whereisthegooglecar.com or
+              any of its owners?
             </h2>
             <p>No.</p>
           </span>
--- site/src/pages/spotting/[id].tsx
+++ site/src/pages/spotting/[id].tsx
@@ -132,7 +132,6 @@ export const getStaticProps = async ({
     } catch (error) {
       return {
         notFound: true,
-        error: error,
       };
     }
   }
