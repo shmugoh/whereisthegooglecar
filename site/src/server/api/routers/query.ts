c3960bd6c532056c24f29a3e1158065a7e804c55
Juan P. <juan@shmugo.co>
2024-06-14 18:50:47 -0500

    [WEB] Merge queryByFilterMonth with queryByMonth

--- site/src/server/api/routers/query.ts
+++ site/src/server/api/routers/query.ts
@@ -16,6 +16,7 @@ BigInt.prototype.toJSON = function () {
 };
 
 export const queryRouter = createTRPCRouter({
+  // grab available months
   queryByFilterMonth: publicProcedure
     .input(
       z.object({
@@ -156,119 +157,73 @@ export const queryRouter = createTRPCRouter({
       return data;
     }),
 
-  queryByFilter: publicProcedure
-    .input(
-      z.object({
-        startDate: z.date(),
-        endDate: z.date(),
-
-        company: z.string().toLowerCase().optional(),
-        country: z.string().toUpperCase().optional(),
-
-        town: z.string().optional(),
-
-        page: z.number().default(0),
-      }),
-    )
-    .mutation(async ({ input, ctx }) => {
-      const { company, startDate, endDate, town, country } = input;
-      const PAGE_SKIP = input.page * PAGE_TAKE;
-
-      const whereClause: any = {};
-
-      if (company) {
-        whereClause.company = company;
-      }
-
-      if (startDate && endDate) {
-        // set hours to 0 to ignore timezones on backend
-        startDate.setHours(0, 0, 0, 0);
-        endDate.setHours(0, 0, 0, 0);
-
-        const newStartDate = new Date(
-          startDate.getTime() - startDate.getTimezoneOffset() * 60000,
-        );
-        const newEndDate = new Date(
-          endDate.getTime() - endDate.getTimezoneOffset() * 60000,
-        );
-        whereClause.date = { gte: newStartDate, lte: newEndDate };
-      }
-
-      if (town) {
-        whereClause.town = {
-          contains: town,
-          mode: "insensitive",
-        };
-      }
-
-      if (country) {
-        whereClause.country = country === "OTHERS" ? "others" : country;
-      }
-
-      const data = await ctx.db.spottings
-        .findMany({
-          where: whereClause,
-          orderBy: { date: "desc" },
-          take: PAGE_TAKE,
-          skip: PAGE_SKIP,
-        })
-        .then((data) => {
-          if (data.length === 0) {
-            throw new TRPCError({
-              code: "NOT_FOUND",
-              message: `No data found`,
-            });
-          } else {
-            return data;
-          }
-        });
-
-      return data;
-    }),
-
+  // query by month
   queryByMonth: publicProcedure
     .input(
       z.object({
+        month: z.number(),
+        year: z.number(),
+
         company: z.string().toLowerCase(),
-        month: z.string(),
-        year: z.string(),
+        town: z.string().optional(),
+        country: z.string().toUpperCase().optional(),
+
         page: z.number().default(0),
+        cache: z.boolean().default(true),
       }),
     )
     .mutation(async ({ input, ctx }) => {
       const { company, month, year, page } = input;
       const PAGE_SKIP = input.page * PAGE_TAKE;
 
-      // // obtains from cache
-      const cachedValue = await kv.hget(
-        `spottings:${company}:${month}:${year}`,
-        `${page}`,
-      );
-      if (cachedValue) {
-        console.log(
-          `[CACHE - queryByMonth] Loading spottings:${company}:${month}:${year}:${page} from cache...`,
+      // obtains from cache
+      if (input.cache) {
+        const cachedValue = await kv.hget(
+          `spottings:${company}:${month}:${year}`,
+          `${page}`,
         );
-        return cachedValue;
+        if (cachedValue) {
+          console.log(
+            `[CACHE - queryByMonth] Loading spottings:${company}:${month}:${year}:${page} from cache...`,
+          );
+          return cachedValue;
+        }
       }
 
       // loads from database if cache is empty
       console.log(
         `[DB - queryByMonth] Loading spottings:${company}:${month}:${year}:${page} from DB...`,
       );
-      const startDate = new Date(
-        Date.UTC(parseInt(year), parseInt(month) - 1, 1),
-      );
-      const endDate = new Date(Date.UTC(parseInt(year), parseInt(month), 0));
 
+      // build where clause
+      const whereClause: any = {};
+      if (typeof month !== "undefined" && typeof year !== "undefined") {
+        const startDate = new Date(Date.UTC(year, month - 1, 1));
+        const endDate = new Date(Date.UTC(year, month, 0));
+
+        whereClause.date = { gte: startDate, lte: endDate };
+      }
+      if (company) {
+        whereClause.company === "others_rest"
+          ? { notIn: ["google", "apple", "yandex"] }
+          : company;
+      }
+      /// search queries
+      if (input.town) {
+        whereClause.town = {
+          contains: input.town,
+          mode: "insensitive",
+        };
+      }
+      if (input.country) {
+        whereClause.country =
+          input.country === "OTHERS" ? "others" : input.country;
+      }
+
+      // perform search to database
       const data = await ctx.db.spottings
         .findMany({
-          where: {
-            company:
-              company === "others_rest"
-                ? { notIn: ["google", "apple", "yandex"] }
-                : company,
-            date: { gte: startDate, lte: endDate },
-          },
+          where: whereClause,
           orderBy: [{ date: "desc" }, { id: "asc" }],
           take: PAGE_TAKE,
           skip: PAGE_SKIP,
@@ -285,12 +240,14 @@ export const queryRouter = createTRPCRouter({
         });
 
       // registers to cache
-      console.log(
-        `[CACHE - queryByMonth] Caching spottings:${company}:${month}:${year}:${page}...`,
-      );
-      await kv.hset(`spottings:${company}:${month}:${year}`, {
-        [page]: data,
-      });
+      if (input.cache) {
+        console.log(
+          `[CACHE - queryByMonth] Caching spottings:${company}:${month}:${year}:${page}...`,
+        );
+        await kv.hset(`spottings:${company}:${month}:${year}`, {
+          [page]: data,
+        });
+      }
 
       // load from database
       return data;
